d89b545fc2d7b801360b99f2d38a191f872c6d61
Miguel López <miguellopez@vitalware.com>
2019-07-18 23:30:30 -0500

    firebase config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyC5oplqAKyX5oYGUmSaHArx5Nia21SqHfo',
+    authDomain: 'firechat-e85f0.firebaseapp.com',
+    databaseURL: 'https://firechat-e85f0.firebaseio.com',
+    projectId: 'firechat-e85f0',
+    storageBucket: '',
+    messagingSenderId: '986651627989',
+    appId: '1:986651627989:web:950f9bd4807ae0c4'
+  }
 };
 
 /*
