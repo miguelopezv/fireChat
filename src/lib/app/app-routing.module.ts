4f13c7b6feee4e5bcef28263431ad092e6ea9438
Miguel López <miguellopez@vitalware.com>
2019-07-19 18:29:26 -0500

    add redirect

--- src/lib/app/app-routing.module.ts
+++ src/lib/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {
+  canActivate,
+  redirectLoggedInTo,
+  redirectUnauthorizedTo
+} from '@angular/fire/auth-guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    ...canActivate(redirectUnauthorizedTo(['login'])),
+    loadChildren: () => import('../chat/chat.module').then(m => m.ChatModule)
+  },
+  {
+    path: 'login',
+    ...canActivate(redirectLoggedInTo([''])),
+    loadChildren: () => import('../login/login.module').then(m => m.LoginModule)
+  },
+  { path: '**', redirectTo: 'login' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
