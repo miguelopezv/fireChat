8b75a22a272c09830d1cddc66bf8e1081db2aeca
Miguel López <miguellopez@vitalware.com>
2019-07-19 18:32:30 -0500

    barrel

---
+++ src/lib/login/containers/index.ts
@@ -0,0 +1,5 @@
+import { LoginLayoutComponent } from './login-layout/login-layout.component';
+
+export const containers = [LoginLayoutComponent];
+
+export * from './login-layout/login-layout.component';
