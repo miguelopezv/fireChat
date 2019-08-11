2977f3ea0dc10c4794d96f63d4dfbd82a883f350
Miguel López <miguellopez@vitalware.com>
2019-08-10 21:00:47 -0500

    add config to prod environmnet

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyC5oplqAKyX5oYGUmSaHArx5Nia21SqHfo',
+    authDomain: 'firechat-e85f0.firebaseapp.com',
+    databaseURL: 'https://firechat-e85f0.firebaseio.com',
+    projectId: 'firechat-e85f0',
+    storageBucket: '',
+    messagingSenderId: '986651627989',
+    appId: '1:986651627989:web:950f9bd4807ae0c4'
+  }
 };
